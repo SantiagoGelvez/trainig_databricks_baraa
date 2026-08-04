7dc60bdc2e5fa361614ba10b040f285218c79636
data@santiagogelvez.com <data@santiagogelvez.com>
2026-08-04 12:09:59 +0000

    Adding silver tables customers and locations

--- bike_lakehouse_2026/common/io.py
+++ bike_lakehouse_2026/common/io.py
@@ -3,7 +3,7 @@ from pyspark.sql import DataFrame
 def read_table(spark, table_name: str) -> DataFrame:
     return spark.table(table_name)
 
-def write_silver(df: DataFrame, table_name: str, mode: str = "overwrite") -> None:
+def write_table(df: DataFrame, table_name: str, mode: str = "overwrite") -> None:
     (
         df.write
         .mode(mode)
